74d235ed1ec08c36272c171a584f6bde093f57c8
Sandeep Maharjan <sandeepmaharjan55@gmail.com>
2021-01-27 23:40:41 +0545

    map route changed

--- WebApplication-musicstore-codefirst/App_Start/RouteConfig.cs
+++ WebApplication-musicstore-codefirst/App_Start/RouteConfig.cs
@@ -13,11 +13,17 @@ namespace WebApplication_musicstore_codefirst
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+           /* routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            );*/
+            routes.MapRoute(
+    "Default",
+    "{controller}/{action}/{id}",
+    new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+    new[] { "WebApplication_musicstore_codefirst.Controllers" }
+);
         }
     }
 }
--- WebApplication-musicstore-codefirst/Areas/Admin/AdminAreaRegistration.cs
+++ WebApplication-musicstore-codefirst/Areas/Admin/AdminAreaRegistration.cs
@@ -14,11 +14,17 @@ namespace WebApplication_musicstore_codefirst.Areas.Admin
 
         public override void RegisterArea(AreaRegistrationContext context) 
         {
-            context.MapRoute(
+           /* context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
                 new { action = "Index", id = UrlParameter.Optional }
-            );
+            );*/
+            context.MapRoute(
+    "Admin_default",
+    "Admin/{controller}/{action}/{id}",
+    new { action = "Index", id = UrlParameter.Optional },
+    new[] { "WebApplication_musicstore_codefirst.Areas.Admin.Controllers" }
+);
         }
     }
 }
